6a2a5e47b6794d46ab539cbd3340d2677182b042
Ricardo Rodriguez <ricardoandres749@gmail.com>
2017-01-26 02:39:25 -0300

    git add .

--- js/game.js
+++ js/game.js
@@ -1,55 +1,90 @@
 $(document).ready(function(){
 
-var contador = 0;
-var img_circle = "img/circle-icon.png";
-var img_x = "img/x-icon.png";
-
-
-if($('#block1') == null){
-  alert('hola');
-}
-  
-
-swal({
-  title: "Bienvenido a tic tac toe!",
-  text: "Jugador 1 escribe tu nombre:",
-  type: "input",
-  showCancelButton: true,
-  closeOnConfirm: false,
-  animation: "slide-from-top",
-  inputPlaceholder: "Nombre"
-},
-function(inputValue){
-  if (inputValue === false)
-    
-      return false;
-  
-  if (inputValue === "") {
-    swal.showInputError("Vamos! tienes que escribir algo...");
-    return false
-  }
-  
-    swal("Excelente!", "Empezemos!", "success")
- 
-  
-});
-
+        var contador = 0;
+        var img_circle = "img/circle-icon.png";
+        var img_x = "img/x-icon.png";
+        var player1 = "";  
+        var player2 = "";  
 
  
 
+        swal({
+            title: "Bienvenido a tic tac toe!",   
+            text: "Jugador 1 escribe tu nombre :",   
+            type: "input",
+            showCancelButton: true,   
+            closeOnConfirm: false,   
+            animation: "slide-from-top",   
+            inputPlaceholder: "Nombre" 
+        },
+        function(inputValue){
+            if (inputValue === false)
+                return false;      
+            if (inputValue === "") {
+                swal.showInputError("Vamos! tienes que escribir algo...");    
+                return false;
+            }
+            if(inputValue.length > 10){
+                swal.showInputError("Espera! tu nombre solo puede tener un maximo de 10 caracteres...");  
+                return false;
+            }
+             player1 = inputValue;
+             $('.player1').text(player1);
+
+            swal({
+                title: "Bienvenido a tic tac toe!",   
+                text: "Jugador 2 escribe tu nombre :",   
+                type: "input",
+                showCancelButton: true,   
+                closeOnConfirm: false,   
+                animation: "slide-from-top",   
+                inputPlaceholder: "Nombre" 
+            },
+            function(inputValue){
+                if (inputValue === false)
+                    return false;      
+                if (inputValue === "") {     
+                    swal.showInputError("Vamos! tienes que escribir algo...");  
+                    return false;
+                }
+                if(inputValue.length > 10){
+                swal.showInputError("Espera! tu nombre solo puede tener un maximo de 10 caracteres...");  
+                return false;
+  } 
+
+                   swal("Excelente! " + "Muy Bien!", "Empezemos...", "success");
+                   player2 = inputValue;
+                   $('.player2').text(player2);
+            });
+        });                 
 
 
-$('.conf').click(function(){
-           
- 
-  swal({
-  title: "<small>Intrucciones</small>!",
-  text: "El Jugador 1 :  Parte con la 'x'. <br> El Jugador 2 : Parte con el 'o'. ",
-  html: true
-});
- 
-     
-});
+
+
+
+            $('.conf').click(function(){
+                    
+            
+            swal({
+            title: "<small>Intrucciones</small>!",
+            text: "El Jugador 1 :  Parte con la 'x'. <br> El Jugador 2 : Parte con el 'o'. ",
+            html: true
+            });
+            
+                
+            });
+
+            $('.btn-reset').click(function(){
+                    
+            $(".block img").remove();
+            $('.block').removeClass('check-o');
+            $('.block').removeClass('check-x');
+            $('.block').css("pointer-events","auto");
+
+            contador = 0;
+
+                
+            });
 
 
 
@@ -57,39 +92,165 @@ $('.conf').click(function(){
 
 
 
-$('.block').click(function() {
- 
 
-contador = contador + 1;
+        $('.block').click(function() {
+        
+
+        contador = contador + 1;
 
 
 
-if (contador%2==0){
-     
-   
-     if($(this).prop("id").length > 0){
+        if (contador%2==0){
+            
+        
+            if($(this).prop("id").length > 0){
 
-      $(this).html("<img class='circle-icon' src='"+img_circle+"' height='145px' width='145px'>");
-      $(this).addClass('check-o');
-      $(this).css("pointer-events","none");
-         
-  
+            $(this).html("<img class='circle-icon' src='"+img_circle+"' height='145px' width='145px'>");
+            $(this).addClass('check-o');
+            $(this).css("pointer-events","none");
+            
+        
+            /*  if($('#block1').hasClass('check-o') && $('#block2').hasClass('check-o') && $('#block3').hasClass('check-o') ){
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+
+            }
+
+
+                if($('#block4').hasClass('check-o') && $('#block5').hasClass('check-o') && $('#block6').hasClass('check-o') ){
+                    
+                swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+            }
+
+
+            
+                if($('#block7').hasClass('check-o') && $('#block8').hasClass('check-o') && $('#block9').hasClass('check-o') ){
+                    
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+            }
+            
+                if($('#block1').hasClass('check-o') && $('#block4').hasClass('check-o') && $('#block7').hasClass('check-o') ){
+                    
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+
+            }
+
+                if($('#block2').hasClass('check-o') && $('#block5').hasClass('check-o') && $('#block8').hasClass('check-o') ){
+                    
+                    
+                swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+            }
+
+            
+                if($('#block3').hasClass('check-o') && $('#block6').hasClass('check-o') && $('#block9').hasClass('check-o') ){
+                    
+                    
+                    sswal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+            }
+
+                    if($('#block1').hasClass('check-o') && $('#block5').hasClass('check-o') && $('#block9').hasClass('check-o') ){
+                    
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+
+            } 
+
+
+                    if($('#block3').hasClass('check-o') && $('#block5').hasClass('check-o') && $('#block7').hasClass('check-o') ){
+                    
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player2 + "", "success");
+
+
+            }*/
+
+        
+        
+            
+            }
+
+        }else{
       
-     }
-
-}else{
-      
     
 
-     if($(this).prop("id").length > 0){
+            if($(this).prop("id").length > 0){
+            
+        
+            $(this).html ("<img class='x-icon' src='"+img_x+"' height='200px' width='150px'>");
+            $(this).addClass('check-x');
+            $(this).css("pointer-events","none");
+
+            if($('#block1').hasClass('check-x') && $('#block2').hasClass('check-x') && $('#block3').hasClass('check-x') ){
+                    
+                    swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+                
+
+
+            }
+
+
        
- 
-       $(this).html ("<img class='x-icon' src='"+img_x+"' height='200px' width='150px'>");
-       $(this).addClass('check-x');
-       $(this).css("pointer-events","none");
+                    if($('#block4').hasClass('check-x') && $('#block5').hasClass('check-x') && $('#block6').hasClass('check-x') ){
+                        
+                            swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
 
-    
-   
+
+                }
+
+
+                
+                    if($('#block7').hasClass('check-x') && $('#block8').hasClass('check-x') && $('#block9').hasClass('check-x') ){
+                        
+                            swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+
+                }
+                
+                    if($('#block1').hasClass('check-x') && $('#block4').hasClass('check-x') && $('#block7').hasClass('check-x') ){
+                        
+                        swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+
+                }
+
+                    if($('#block2').hasClass('check-x') && $('#block5').hasClass('check-x') && $('#block8').hasClass('check-x') ){
+                        
+                        swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+
+                }
+
+                
+                    if($('#block3').hasClass('check-x') && $('#block6').hasClass('check-x') && $('#block9').hasClass('check-x') ){
+                        
+                        swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+                }
+
+                        if($('#block1').hasClass('check-x') && $('#block5').hasClass('check-x') && $('#block9').hasClass('check-x') ){
+                        
+                        swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+
+                } 
+
+
+                        if($('#block3').hasClass('check-x') && $('#block5').hasClass('check-x') && $('#block7').hasClass('check-x') ){
+                        
+                        swal("Genial!", "Ganaste!!!" + " " + player1 + "", "success");
+
+                }
+
+           
 
        }   
 
@@ -100,57 +261,4 @@ if (contador%2==0){
 
    
 });
-
-
-
-
-
-
-
-    
-    /*var block1 =  document.getElementById('block1');
-    var block2 =  document.getElementById('block2');
-    var block3 =  document.getElementById('block3');
-    var block4 =  document.getElementById('block4');
-    var block5 =  document.getElementById('block5');
-    var block6 =  document.getElementById('block6');
-    var block7 =  document.getElementById('block7');
-    var block8 =  document.getElementById('block8');
-    var block9 =  document.getElementById('block9');
-
-
-
-  alert("el numero es impar");
-  
-  if(block1.innerHTML == '1'){
-        $("#block1").text('x');
-  }
-   if(block2.innerHTML == '2'){
-        $("#block2").text('x');
-  }
-   if(block3.innerHTML == '3'){
-        $("#block3").text('x');
-  }
-   if(block4.innerHTML == '4'){
-        $("#block4").text('x');
-  }
-   if(block5.innerHTML == '5'){
-        $("#block5").text('x');
-  }
-   if(block6.innerHTML == '6'){
-        $("#block6").text('x');
-  }
-   
-   if(block7.innerHTML == '7'){
-        $("#block7").text('x');
-  }
-
-   if(block8.innerHTML == '8'){
-        $("#block8").text('x');
-  }
-
-   if(block9.innerHTML == '9'){
-        $("#block9").text('x');
-  } */
-
 
